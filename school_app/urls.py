3fc19f45db4fc1d799850533e3bb19fa5718aa0b
Marc-Ilunga Caiphe <caipheilunga@gmail.com>
2020-02-26 06:20:32 +0200

    Urls Update

--- school_app/urls.py
+++ school_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.homeView, name="Welcome-Home"),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
